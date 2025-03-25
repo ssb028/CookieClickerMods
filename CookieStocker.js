6c3201eb7ede0566bb05478721cc86ca45f27802
ssb028 <102758891+ssb028@users.noreply.github.com>
2025-03-25 14:08:17 -0500

    Update CookieStocker.js

--- CookieStocker.js
+++ CookieStocker.js
@@ -41,7 +41,7 @@
 // ===================================================================================
  
 if(typeof CCSE == undefined)
-    Game.LoadMod('https://klattmose.github.io/CookieClicker/SteamMods/CCSE/main.js')
+    Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js')
  
 if(CookiStocker === undefined) var CookiStocker = {};
  
